ac028195595926d458e0767965cc8afb81ec5cad
JeremiasOlschansky <jolschansky@frba.utn.edu.ar>
2025-04-12 12:42:41 +0000

    Agrego funciones al utils

--- utils/src/utils/hello.c
+++ utils/src/utils/hello.c
@@ -6,7 +6,7 @@ void saludar(char* quien) {
 }
 // FUNCIONES PARA LOS CONFIGS
 t_config *crear_config(char* direccion){
-    t_config *nuevo_config = config_create(direccion);
+    t_config *nuevo_config = config_create(direccion); // se pasa la ruta del archivo .config
     if(!nuevo_config){
         perror("No se pudo crear el config");
         exit(EXIT_FAILURE);
@@ -31,4 +31,78 @@ t_log *iniciar_logger(char* archivoLog, char* nombreLog){
 
 void destruir_logger(t_log *logger){
     log_destroy(logger);
+}
+
+//funcion del cliente
+
+int crear_conexion(char *ip, char* puerto)
+{
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(ip, puerto, &hints, &server_info);
+
+	int socket_cliente = socket (server_info->ai_family,
+								server_info->ai_socktype,
+								server_info->ai_protocol);
+connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+
+
+	freeaddrinfo(server_info);
+
+	return socket_cliente;
+}
+
+// FUNCIONES PARA EL SERVIDOR 
+int iniciar_servidor(char *puerto,t_log *un_log, char *mensaje)
+{
+	struct addrinfo hints, *servinfo; //*p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	getaddrinfo(NULL, puerto, &hints, &servinfo);
+
+	
+ int socket_servidor = socket (servinfo->ai_family,
+								servinfo->ai_socktype,
+								servinfo->ai_protocol);
+	
+bind(socket_servidor,servinfo->ai_addr,servinfo->ai_addrlen);
+	
+listen(socket_servidor,SOMAXCONN);
+
+	freeaddrinfo(servinfo);
+	log_trace(un_log, "Server: %s", mensaje);
+
+	return socket_servidor;
+}
+
+int esperar_cliente(int socket_servidor, t_log *un_log)
+{
+	
+	int socket_cliente = accept(socket_servidor, NULL, NULL);;
+	log_info(un_log, "Se conecto un cliente!");
+
+	return socket_cliente;
+}
+
+
+int recibir_operacion(int socket_cliente)
+{
+	int cod_op;
+	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+		return cod_op;
+	else
+	{
+		close(socket_cliente);
+		return -1;
+	}
 }
--- utils/src/utils/hello.h
+++ utils/src/utils/hello.h
@@ -10,16 +10,42 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <commons/config.h>
-
-
+#include <string.h> // para usar, por ejemplo, la funcion memset en iniciar conexion
+#include <unistd.h> // para funciones como por ejemplo close(linea 105 hello.c)
 /**
 * @brief Imprime un saludo por consola
 * @param quien Módulo desde donde se llama a la función
 * @return No devuelve nada
 */
+typedef enum
+{
+	MENSAJE,
+	PAQUETE,
+    HANDSHAKE,
+    CREAR_PROCESO, // ESTE Y EL DE ABAJO SE PODRIAN CREAR PARA CADA CONEXION TIPO CREAR_PROCESO_KM (DE KERNEL A MEMORIA)
+    RTA,CREAR_PROCESO // RTA MK SERIA LA RTA QUE LA MEMORIA LE PASA AL KERNEL
+}op_code;
+
+typedef struct
+{
+	int size;
+	void* stream;
+} t_buffer;
+
+typedef struct
+{
+	op_code codigo_operacion;
+	t_buffer* buffer;
+} t_paquete;
+
+
 void saludar(char* quien);
 t_config *crear_config(char* direccion);
 void destruir_config(t_config *config);
 t_log *iniciar_logger(char* archivoLog, char* nombreLog);
 void destruir_logger(t_log *logger);
+int crear_conexion(char *ip, char* puerto);
+int iniciar_servidor(char *puerto,t_log *un_log, char *mensaje);
+int esperar_cliente(int socket_servidor, t_log *un_log);
+int recibir_operacion(int socket_cliente);
 #endif
