8101c36c982a8fefa49083a5e12e6674fa718d3a
JeremiasOlschansky <jolschansky@frba.utn.edu.ar>
2025-04-23 19:29:30 +0000

    falta arreglar lo del tiempo de espera

--- io/src/io_conexiones.c
+++ io/src/io_conexiones.c
@@ -12,6 +12,7 @@ void esperar_peticion(){
     while (1){
         printf("Entre al while y estoy esperando \n");
         tiempo_de_suspension = recibir_entero(fd_kernel);
+        printf("\n");
        if(tiempo_de_suspension>0){
         ejecutarPeticion(tiempo_de_suspension) ;
         tiempo_de_suspension  = 0;
@@ -21,7 +22,8 @@ void esperar_peticion(){
 }
 //el sistema esta bloqueado hasta que llega la peticion que lo saca de la suspension. 
 void ejecutarPeticion(int tiempo){
-    printf("En proceso y recibi: %d  /n", tiempo);
+    printf("En proceso y recibi: %d ", tiempo);
+    printf("\n");
     usleep(tiempo*1000000); //es como que procesa el tiempo que dura la peticion
     //avisar_que_finalice()
 }
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -6,8 +6,8 @@ int main(int argc, char* argv[]) {
     inicializar_kernel();
     //atender_kernel_dispatch();
     atender_kernel_io();
-
     enviar_entero(fd_io,3);
+    
     printf("Programa Finalizado");
 }
 
