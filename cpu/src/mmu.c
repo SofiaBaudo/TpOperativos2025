81835c4092027c739d23e0adf88417a66c08cea8
SofiaBaudo <sofibaudo@gmail.com>
2025-06-01 15:09:54 +0000

    modificaciones en mmu

--- cpu/src/mmu.c
+++ cpu/src/mmu.c
@@ -6,28 +6,30 @@ Las direcciones logicas estarian compuestas de esta manera.
 ¿SE PODRIA PENSAR CON ARBOLES?
 EL desplazameinto dice cuantos bits avanzar hasta la direccion que queres. 
 */
-
-struct pagina{
-    int numpag;
-    //int tamPag; el tamPag lo tendriamos que pasar en el handshake con memoria. Entonces lo voy a poner ocmo una variable global y vemos
-}
-
+//LA DIRECCION NOS LA DA EL PARAMETRO DE LAS INSTRUCCIONES
+//EL TAMAÑO DE LA PAG TE LA DA EL HANDSJAKE
 struct nivel{
     int numeroNivel;
     int tamanio;
     void* tablaDeNivel[400]; //es puntero generico porque hay veces que apunta a una pagina y veces que apunta a un prox nivel
 }
-//uint32_t es el tipo de dato que se suele hacer para la direccion de datos, es para 32 bits. 
 
 void creacionNivelesN(int n){
    nivel* niveles[n]; 
    //es una tabla de n niveles donde cada nivel tiene el tipo de dato nivel. 
 }
-uint32_t traduccion(int desplazamiento, nivel niveles[], int cantNiveles, uint32_t dirLogica){
-    pagina pagina;
-    pagina.numpag = floor(dirLogica / tamPag);
-    int entrada_nivel; 
-    //no sabria como encararlo --> que tipo de dato seria? que es lo que tiene dentro.
-    int desplazamiento = dirLogica % tamPag;
-    //el desplazamiento se hace en cada nivel?
+
+int traduccion(int direccion){ //te tendria que devolver la dir fisica
+    int numPag = floor(direccion/tamPag);
+    int desplazamiento = direccion % tamPag; 
+    int numeroMarco;
+    numeroMarco = accederATP(numPag);
+    //SEGUIR
+}
+
+int accederATp(int numPag){
+    int numMarco = 0;
+    send(fd_conexion_dispatch_memoria, &numPag, sizeof(int), 0);
+    recv(fd_conexion_dispatch_memoria, &numMarco, sizeof(int), 0);
+    return numMarco;
 }
